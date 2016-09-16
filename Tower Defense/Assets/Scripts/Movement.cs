97f0b73f270223e461b9b7cf68ccfa86a66bf320
Zheman Shi <zhemansh@usc.edu>
2016-09-15 19:08:44 -0700

    Prototype startup

---
+++ Tower Defense/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+		target = Turret.turretWaypoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!dead){
+			Vector3 dir = target.position - transform.position;
+			dir.y = 0;
+			if(dir.magnitude > .2){
+				transform.Translate (dir.normalized * speed * Time.deltaTime);
+			}
+			else{
+				dead = true;
+			}
+		}
+	}
+}
---
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	public static Transform turretWaypoint;
+
+	// Use this for initialization
+	void Start () {
+		turretWaypoint = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
