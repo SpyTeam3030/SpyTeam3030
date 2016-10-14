126c74fee3d07d8ef5aba5d880c38679373c6c56
Zheman Shi <zhemansh@usc.edu>
2016-10-13 17:27:24 -0700

    Cards UI

---
+++ SpyTeam3030_Prototye/Assets/Script/DragHandeler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition;
+	Transform startParent;
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+//		print (Input.mousePosition);
+//		print (transform.position);
+		this.transform.position = eventData.position;
+//		Vector3 newZ = this.transform.position;
+//		newZ.z = newZ.z - 10;
+//		this.transform.position= newZ;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = null;
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		transform.position = startPosition;
+	}
+
+	#endregion
+
+}
